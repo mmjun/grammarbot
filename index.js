8c46f826d7929c080c35676708c4be3eb0446397
Mike Jun <jun@M1-Maximum.local>
2026-05-04 14:23:58 -0700

    Fix CORS order

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, x-api-key");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // ----------------- SHARED BLOCK GENERATOR -----------------
 function buildMessageBlocks(corrected) {
